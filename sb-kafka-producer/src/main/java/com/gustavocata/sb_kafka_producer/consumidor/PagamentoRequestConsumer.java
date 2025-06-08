1f2c43c8c682597a9434cb3dcf48544fbe5a7a08
gustavocata <gustavo.catarino10@hotmail.com>
2025-06-08 00:09:51 -0300

    "Criação do pagamento, bem como seu envio e consumo por parte do kafka"

---
+++ sb-kafka-producer/src/main/java/com/gustavocata/sb_kafka_producer/consumidor/PagamentoRequestConsumer.java
@@ -0,0 +1,17 @@
+package com.gustavocata.sb_kafka_producer.consumidor;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PagamentoRequestConsumer {
+
+    @KafkaListener(
+            topics = "${topicos.pagamento.request.topic}",
+            groupId = "pagamento-request-consumer-1"
+    )
+    public void consume (String message){
+        System.out.println(" =======MENSAGEM RRCEBIDA ===== " + message);
+    }
+
+}
